7ddb4b1d49ddfaf5953ca2b3bcbb4db3dfd5aef8
Kylmakalle <iceman9831@gmail.com>
2019-06-30 14:56:30 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Sergey on 30/06/2019.
+//  Copyright © 2019 Sergey Akentev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
